f6ab4611e48f672cd13fc81e58112f41a6b3af92
Richard Zheng <rzheng14@binghamton.edu>
2020-02-19 15:27:37 -0500

    Initial Commit

---
+++ Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picker
+//
+//  Created by Richard Zheng on 2/19/20.
+//  Copyright © 2020 Richard Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
